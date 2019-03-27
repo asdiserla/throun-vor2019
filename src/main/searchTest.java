2b9eee0d0ceb72971113ff187402a987aad00b4b
hthg13 <32647181+hthg13@users.noreply.github.com>
2019-03-27 20:07:38 +0000

    fleyri klasar

--- src/main/searchTest.java
+++ src/main/searchTest.java
@@ -1,38 +1,3 @@
-package main;
-
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-
 /**
- * return null when nothing found
- * return ArrayList when something found
- * return error message when input to method is not a TourFilter
+ * Test eru undir out/test/throun/main
  */
-
-public class searchTest {
-
-    public void SearchClassTest() {
-    }
-
-    @BeforeClass
-    public static void setUpClass() {
-    }
-
-    @AfterClass
-    public static void tearDownClass() {
-    }
-
-    @Before
-    public void setUp() {
-    }
-
-    @After
-    public void tearDown() {
-    }
-
-    @Test
-    public void hello() {}
-}
