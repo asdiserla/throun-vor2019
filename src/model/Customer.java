52de1ca328f6f699ef6680048a719720ff81aa24
Ásdís Erla <asdiserla@Asdiss-MacBook-Pro.local>
2019-04-07 23:00:22 +0000

    fikta i vidmoti fyrir search

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -11,6 +11,15 @@ public class Customer {
     public int tourPickID;       // helga added this, should we not save the tour that the customer picks?
     public Booking booking;
 
+    /**
+     * Constructor for Customer class
+     * @param id
+     * @param firstName
+     * @param lastName
+     * @param email
+     * @param mobile
+     * @param filter
+     */
     public Customer(String id, String firstName, String lastName, String email,
                     String mobile, TourFilter filter, int tourPickID) {
         this.id = id;
