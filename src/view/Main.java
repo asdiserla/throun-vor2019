fe5e7c9e84c41462d6022b282d82e6f1a5c63b7e
hthg13 <32647181+hthg13@users.noreply.github.com>
2019-04-08 10:49:31 +0000

    bætt við gagnagrunninn 10 ferðum sem passa við dagsetningar hjá F hópnum

--- src/view/Main.java
+++ src/view/Main.java
@@ -8,12 +8,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import model.Customer;
-import model.Tour;
-import model.TourFilter;
-
-import java.util.Calendar;
-import java.util.Date;
-import java.util.LinkedList;
 
 public class Main extends Application {
 
